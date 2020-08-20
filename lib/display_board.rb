62f7bc00fe0c37188bf8911cc927a3581aa1990a
Kevin Glasgow <kglas001@gmail.com>
2020-08-20 19:29:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]}| #{borad[1]} | #{borad[2] ""
   puts "-----------"
-  puts " #{board[3]} | #{borad[4]} | #{borad[5] ""
+  puts " #{board[3]} | #{borad[4]} | #{borad[5] 
   puts "-----------"
   puts " #{board[6]}  | #{borad[7]}  |  #{borad[8]} "
 end 
