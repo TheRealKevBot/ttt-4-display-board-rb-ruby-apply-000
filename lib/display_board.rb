a78f0fb1b18249301e873aa00c3a9e46f13ecdc5
Kevin Glasgow <kglas001@gmail.com>
2020-08-20 19:32:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[0]}| #{borad[1]} | #{borad[2] ""
+  puts " #{board[0]}  | #{board[1]}  |  #{board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{borad[4]} | #{borad[5] 
+  puts " #{board[3]} |  #{board[4]} | #{board[5]}  "
   puts "-----------"
   puts " #{board[6]}  | #{borad[7]}  |  #{borad[8]} "
 end 
